6333b4afcbf35a5a1dc3cde260990e032b55dcf4
waleedaldobie-rgb <waleedaldobie@gmail.com>
2026-08-18 22:51:10 +0300

    perf(sqlite): add index performance integration test (#5)
    Merged after successful quality, unit, integration, SQLite index performance, and Windows UI automation checks.

--- src/electron/migrations/index.ts
+++ src/electron/migrations/index.ts
@@ -4,11 +4,13 @@ import { migration002 } from './002_order_references';
 import { migration003 } from './003_order_events_indexes';
 import { migration004 } from './004_order_events_created_at';
 import { migration005 } from './005_performance_indexes';
+import { migration006 } from './006_list_order_indexes';
 
 export const MIGRATIONS: Migration[] = [
   migration001,
   migration002,
   migration003,
   migration004,
-  migration005
+  migration005,
+  migration006
 ].sort((a, b) => a.version - b.version);
